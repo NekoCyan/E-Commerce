5691565743b97001c44d3ea9f956316c0da116ab
NekoCyan <cyanhijirikawa88@gmail.com>
2024-03-16 03:28:44 +0700

    fix: read desc (this is important for every coder).
    As https://nextjs.org/docs/messages/static-page-generation-timeout.
    When there's promise, the page will need to wait follow promise and check result (not in case using database).
    So we cannot use long timer.

--- src/app/loading/page.tsx
+++ src/app/loading/page.tsx
@@ -1,7 +1,7 @@
-import { setTimeout } from "timers/promises";
+import { setTimeout } from 'timers/promises';
 
 export default async function LoadingForTest() {
-    await setTimeout(30 * 60 * 1000);
-    
+	await setTimeout(10 * 1000);
+
 	return <div>Hello world!</div>;
 }
