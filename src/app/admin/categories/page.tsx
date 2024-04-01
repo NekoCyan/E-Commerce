312ca2499604afb7bd59191996eaed9e0eaf6ded
NekoCyan <cyanhijirikawa88@gmail.com>
2024-04-02 03:48:52 +0700

    fix: no cache for categories.

--- src/app/admin/categories/page.tsx
+++ src/app/admin/categories/page.tsx
@@ -3,7 +3,7 @@ import Component from './component';
 
 export default async function Page(props: any) {
 	const fetchedCategories = await fetch(getUrl('/api/categories'), {
-		next: { revalidate: 60 },
+		cache: 'no-cache',
 	});
 	const { data } = await fetchedCategories.json();
 
