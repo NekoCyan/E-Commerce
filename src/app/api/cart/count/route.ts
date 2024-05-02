62e2569467f6f13e46b6ea869a834e8700ac3ac1
NekoCyan <cyanhijirikawa88@gmail.com>
2024-05-02 08:11:40 +0700

    feat: optimize & next newest ver.

--- src/app/api/cart/count/route.ts
+++ src/app/api/cart/count/route.ts
@@ -10,13 +10,18 @@ export async function GET(req: NextRequest) {
 			req: req,
 		})) as Session;
 
-		const count = await Cart.aggregate([
-			{ $match: { userId: session.user?.id } },
-			{ $unwind: '$data' },
-			{ $group: { _id: null, dataCount: { $sum: 1 } } },
-		]);
-		if (count.length === 0) return Response({ count: 0 });
-		else return Response({ count: count[0]?.dataCount ?? 0 });
+		// const count = await Cart.aggregate([
+		// 	{ $match: { userId: parseInt(session.user!.id) } },
+		// 	{ $unwind: '$data' },
+		// 	{ $group: { _id: null, dataCount: { $sum: 1 } } },
+		// ]);
+		// if (count.length === 0) return Response({ count: 0 });
+		// else return Response({ count: count[0]?.dataCount ?? 0 });
+
+		const count = await Cart.findOne({
+			userId: parseInt(session.user!.id),
+		});
+		return Response({ count: count?.data.length ?? 0 });
 	} catch (e: any) {
 		return ErrorResponse(e);
 	}
